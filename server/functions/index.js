e8b4aa64ef968042379dc1a11defcb458ea75e9c
Anton Ha <antonha016@gmail.com>
2024-12-03 19:10:40 -0500

    added netlify config

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const serverless = require("serverless-http")
+const router = express.Router();
+
 require('dotenv').config(); // Loads environment variables from .env file
 
 const app = express();
@@ -10,18 +13,15 @@ app.use(cors());
 app.use(bodyParser.json()); // Parses incoming JSON requests
 
 // Example Routes
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
   res.send('Server is running!');
 });
 
-app.post('/api/todos', (req, res) => {
+router.post('/api/todos', (req, res) => {
   const todo = req.body;
   console.log('Received todo:', todo);
   res.status(201).json({ message: 'Todo created', todo });
 });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.use('/.netlify/functions/api',router)
+module.exports.handler = serverless(app)
