492063ecd64a0dd2a56722275362ac7d2bb8ab35
Anton Ha <antonha016@gmail.com>
2024-12-08 17:50:33 -0500

    added server notice startup

--- client/src/components/Pages/Home.tsx
+++ client/src/components/Pages/Home.tsx
@@ -6,15 +6,15 @@ const Home: React.FC = () => {
     const { user } = useSession();
     return (
         <div>
-            <div>
+            <div className="d-flex justify-content-center flex-column align-items-center">
                 <h1>
                     Welcome{user ? `, ${user.username}` : ""}!
                 </h1>
-                <p>
+                <h3>
                     {user
                         ? "We're glad to see you back. Explore your todos and stay organized!"
                         : "Please log in or register to get started with your todos."}
-                </p>
+                </h3>
                 <Todo/>
             </div>
         </div>
--- client/src/components/reuseables/Layout.tsx
+++ client/src/components/reuseables/Layout.tsx
@@ -7,6 +7,7 @@ export const Layout = ({ children} : any) => {
             <section className="main-container">
                 {children}
             </section>
+            <footer><p>After sending the first request to the backend server please wait a minute or two then try again and the backend server needs to be started up after inactive for a period!</p></footer>
         </div>
     )
 }
