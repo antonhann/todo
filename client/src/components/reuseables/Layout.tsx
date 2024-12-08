64a2748dbcc381e3091c1ff03788dd5f39b4d757
Anton Ha <antonha016@gmail.com>
2024-12-08 18:05:00 -0500

    added note

--- client/src/components/reuseables/Layout.tsx
+++ client/src/components/reuseables/Layout.tsx
@@ -7,7 +7,7 @@ export const Layout = ({ children} : any) => {
             <section className="main-container">
                 {children}
             </section>
-            <footer><p>After sending the first request to the backend server please wait a minute or two then try again and the backend server needs to be started up after inactive for a period!</p></footer>
+            <footer><p>After sending the first request to the backend server please wait a minute or two, your request will be fulfilled then the backend will act normally (the backend server needs to be started up after inactive for a period!)</p></footer>
         </div>
     )
 }
